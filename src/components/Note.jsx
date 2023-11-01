d855f4ccafebee4195d6b6f00082f93ce16b8cd4
NarraBhavani <113877880+NarraBhavani@users.noreply.github.com>
2023-11-01 13:24:08 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>FareWell for 2023 Batch!</h1>
+      <p>
+        It's time to say goodbye, and we invite you to join us at
+        RavindraBharathi on 10/10/2023 at 11:00AM to bid farewell to an
+        exceptional colleague. Time doesn't stop for anyone.... Come and
+        participate in this joyous celebration, the first farewell party for
+        Seniors, and create an unforgettable experience.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
